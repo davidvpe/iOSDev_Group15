7d22297c00c4af92b8a3fe5df68db936229592f4
David Velarde <davidvpe@gmail.com>
2016-11-10 22:01:14 -0500

    Agregar Empresa con Unwind Scene

---
+++ module_1/class_8/Chambas/Chambas/DetalleViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DetalleViewController.swift
+//  Chambas
+//
+//  Created by David Velarde on 11/3/16.
+//  Copyright © 2016 Area51. All rights reserved.
+//
+
+import UIKit
+
+class DetalleViewController: UIViewController {
+    
+    @IBOutlet weak var lblNombre : UILabel!
+    @IBOutlet weak var imgEmpresa : UIImageView!
+    
+    var empresaRecibida : Empresa?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+        imgEmpresa.image = UIImage(named: (empresaRecibida?.imagen)!)
+        
+        
+        lblNombre.text = empresaRecibida?.nombre
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let vc = segue.destination as! MapaViewController
+        vc.empresaRecibida = empresaRecibida
+        
+        
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+ 
+
+}
---
+++ module_1/class_8/Chambas/Chambas/EmpresaCell.swift
@@ -0,0 +1,25 @@
+//
+//  EmpresaCell.swift
+//  Chambas
+//
+//  Created by David Velarde on 11/3/16.
+//  Copyright © 2016 Area51. All rights reserved.
+//
+
+import UIKit
+
+class EmpresaCell: UITableViewCell {
+
+    @IBOutlet weak var imgEmpresa: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
