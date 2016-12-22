28ea97cc18650b0a894689c087851f9ef0726493
David Velarde <davidvpe@gmail.com>
2016-12-22 18:56:07 -0500

    Clase 2 Base

---
+++ module_3/class_2/Services/Services/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Services
+//
+//  Created by David Velarde on 12/22/16.
+//  Copyright © 2016 Area51. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import ALLoadingView
+
+
+class ViewController: UIViewController {
+
+    @IBAction func loadData(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
