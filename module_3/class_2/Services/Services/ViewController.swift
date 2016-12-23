d3ca8fb3e0cac3808471b5aa55d3daeb69e7ed40
David Velarde <davidvpe@gmail.com>
2016-12-22 20:53:38 -0500

    Clase completada

--- module_3/class_2/Services/Services/ViewController.swift
+++ module_3/class_2/Services/Services/ViewController.swift
@@ -15,8 +15,71 @@ import ALLoadingView
 class ViewController: UIViewController {
 
     @IBAction func loadData(_ sender: Any) {
+        //https://jsonplaceholder.typicode.com/users
         
+        //Cmd+B
+        
+        ALLoadingView.manager.messageText = "Descargando Usuarios"
+        
+        ALLoadingView.manager.blurredBackground = true
+        
+        ALLoadingView.manager.showLoadingView(ofType: .messageWithIndicator, windowMode: ALLVWindowMode.fullscreen, completionBlock: nil)
+
+        
+        Alamofire.request("https://jsonplaceholder.typicode.com/users", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { (dataResponse) in
+            
+            
+            ALLoadingView.manager.hideLoadingView()
+            
+            switch dataResponse.result {
+            
+            case .success:
+                
+                
+                let json = JSON(data: dataResponse.data!)
+                
+                let arrayUsers = json.arrayValue
+                
+                for jsonUser in arrayUsers{
+                    
+                    let name = jsonUser["name"].stringValue
+                    let email = jsonUser["email"].stringValue
+                    
+                    print("Usuario \(name) con correo \(email)")
+                    
+                }
+                
+                
+                
+            case .failure(let error):
+                
+//                let error2 = error as NSError
+//                
+//                print(error2.code)
+                
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+                
+            }
+            
+            //print(dataResponse)
+            
+        }
+        
+        //Alamofire.request("https://jsonplaceholder.typicode.com/users", method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil)
+        
+        /*Alamofire.request("https://jsonplaceholder.typicode.com/users", method: .get, parameters:
+            [
+                "user":"davidvpe",
+                "password":"123456789"
+            ]
+            , encoding: JSONEncoding.default, headers: nil)
+         */
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
